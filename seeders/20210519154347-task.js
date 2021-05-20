575707191e41d295f3b1f9d234d09ee3dc0f742c
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-05-20 09:55:37 +0200

    defined associations

---
+++ seeders/20210519154347-task.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+  await queryInterface.bulkInsert('Tasks', [{
+        title: 'Build an app',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.bulkDelete('Tasks', null, {});
+  }
+};
