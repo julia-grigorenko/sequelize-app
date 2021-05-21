c70cb9b91b8dd867003a2e9dce0852f01e24c41f
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-05-21 17:53:31 +0200

    add functionality to get all users with their associated tasks, get a specific user and tasklist, update a user, and delete a user.

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const controllers = require('../controllers');
+const router = Router();
+
+router.get('/', (req, res) => res.send('Main page of app'));
+router.post('/users', controllers.createUser);
+router.get('/users', controllers.getAllUsers);
+router.get('/users/:id', controllers.getUserById);
+router.put('/users/:id', controllers.updateUser);
+router.delete('/users/:id', controllers.deleteUser);
+
+
+module.exports = router;
